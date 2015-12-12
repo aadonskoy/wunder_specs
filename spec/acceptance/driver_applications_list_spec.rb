90fddc4ea292408134197b4a8f5e56b860721264
Alexey Donskoy <aadonskoy@gmail.com>
2015-12-12 14:02:38 +0200

    Updates tests for the User model for #name. Adds acceptance test for index page.

---
+++ spec/acceptance/driver_applications_list_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+# View a list of driver applications displaying the applicant's name,
+# email address, city and application status
+
+RSpec.describe 'List of driver applications', type: :feature do
+  let!(:driver_application) { create(:driver_application) }
+  let(:applicant) { driver_application.user }
+  let(:applicant_name) { applicant.name }
+  let(:applicant_email) { applicant.email }
+  let(:applicant_city) { driver_application.city.name }
+  let(:applicant_status) { applicant.status }
+
+  before(:each) do
+    visit '/'
+  end
+
+  subject { page }
+
+  context 'displaying applicant\'s name' do
+    it { is_expected.to have_content(applicant_name) }
+  end
+
+  context 'displaying applicant\'s email' do
+    it { is_expected.to have_content(applicant_email) }
+  end
+
+  context 'displaying applicant\'s city' do
+    it { is_expected.to have_content(applicant_city) }
+  end
+
+  context 'displaying applicant\'s status' do
+    it { is_expected.to have_content(applicant_status) }
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,4 +9,13 @@ RSpec.describe User, type: :model do
   it { is_expected.not_to allow_value('@email.com', 'some_email.com', '123@com').for(:email) }
 
   it { is_expected.to have_one(:driver_application).dependent(:destroy) }
+
+  describe '#name' do
+    let(:user) { create(:user) }
+    let(:first_name) { create(:user) }
+    let(:last_name) { create(:user) }
+
+    subject { user.name }
+    it { is_expected.to eq("#{first_name} #{last_name}") }
+  end
 end
